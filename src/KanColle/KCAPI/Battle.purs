ba272029f8b6f57af8f7878274a65812665a0e6c
Javran Cheng <Javran.c@gmail.com>
2015-07-25 17:37:11 -0400

    battle info extraction

--- src/KanColle/KCAPI/Battle.purs
+++ src/KanColle/KCAPI/Battle.purs
@@ -1,11 +1,18 @@
 module KanColle.KCAPI.Battle where
 
+import Prelude
+import Data.Maybe
 import Data.Foreign
 import Data.Nullable
 import Data.Foreign.Index
+import qualified Data.Array.Unsafe as AU
 
--- things might actually be "null",
--- we choose to trust flags, i.e. api_stage_flag and api_hourai_flag
+-- things might actually be "null" or even "undefined"
+-- we choose to:
+-- * trust flag api_opening_flag for opening torpedo attacks
+-- * test property existency in other cases
+
+-- TODO: change this to foreign, or hide the definition
 type Battle =
   { api_nowhps :: Array Int
   , api_stage_flag :: Array Int
@@ -19,6 +26,7 @@ type Battle =
   , api_hougeki2 :: Hougeki
   , api_hougeki3 :: Hougeki
   , api_raigeki :: Raigeki
+  , api_hougeki :: Hougeki
   }
 
 type NightBattle =
@@ -46,7 +54,7 @@ type Raigeki =
   }
 
 hasField :: String -> Battle -> Boolean
-hasField s b = hasOwnProperty s (toForeign b)
+hasField s = hasOwnProperty s <<< toForeign
 
 hasKouku :: Battle -> Boolean
 hasKouku = hasField "api_stage_flag"
@@ -56,3 +64,48 @@ hasKouku2 = hasField "api_stage_flag2"
 
 hasHourai :: Battle -> Boolean
 hasHourai = hasField "api_hourai_flag"
+
+hasHougeki :: Battle -> Boolean
+hasHougeki = hasField "api_hougeki"
+
+getKouku :: Battle -> Maybe Kouku
+getKouku b = if hasKouku b
+  then Just b.api_kouku
+  else Nothing
+
+getKouku2 :: Battle -> Maybe Kouku
+getKouku2 b = if hasKouku2 b
+  then Just b.api_kouku2
+  else Nothing
+
+getOpeningAttack :: Battle -> Maybe Raigeki
+getOpeningAttack b = 
+    if hasField "api_opening_flag" b
+    && b.api_opening_flag == 1
+      then Just b.api_opening_atack
+      else Nothing
+
+getHougeki1 :: Battle -> Maybe Hougeki
+getHougeki1 b = if hasField "api_hougeki1" b
+  then Just b.api_hougeki1
+  else Nothing
+
+getHougeki2 :: Battle -> Maybe Hougeki
+getHougeki2 b = if hasField "api_hougeki2" b
+  then Just b.api_hougeki2
+  else Nothing
+
+getHougeki3 :: Battle -> Maybe Hougeki
+getHougeki3 b = if hasField "api_hougeki3" b
+  then Just b.api_hougeki3
+  else Nothing
+
+getRaigeki :: Battle -> Maybe Raigeki
+getRaigeki b = if hasField "api_raigeki" b
+  then Just b.api_raigeki
+  else Nothing
+
+getHougeki :: Battle -> Maybe Hougeki
+getHougeki b = if hasHougeki b
+  then Just b.api_hougeki
+  else Nothing
