cd024659a1ba85ae39206dac9e46d3a91cff91c2
Javran Cheng <Javran.c@gmail.com>
2015-08-03 23:13:27 -0400

    force fucking reinstalls

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,7 +5,7 @@ set -x
 cabal update
 cabal install happy --force-reinstalls
 cabal install alex --force-reinstalls
-cabal install purescript-0.7.2.0
+cabal install purescript-0.7.2.0 --force-reinstalls
 
 # dependencies
 npm install pulp
