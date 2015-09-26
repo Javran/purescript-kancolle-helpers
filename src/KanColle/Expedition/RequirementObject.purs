f545526140d92a51bd01ab713e36caca51ce87a1
Javran Cheng <Javran.c@gmail.com>
2015-09-26 15:37:55 -0400

    requirement check reworked

--- src/KanColle/Expedition/RequirementObject.purs
+++ src/KanColle/Expedition/RequirementObject.purs
@@ -4,6 +4,7 @@ import Prelude
 import Data.Foldable
 import Data.Maybe
 import Data.Array
+import Data.Monoid
 import Data.Nullable
 
 import KanColle.SType
@@ -19,16 +20,26 @@ type STypeReqObj =
   { stypeReqCount :: Int
   , stypeOneOf :: Array String
   }
-  
+
 -- compact requirement representation
 type RequirementPack =
   { flagShipLevel :: Int
   , shipCount :: Int
   , flagShipTypeOf :: Maybe (Array SType)
   , levelCount :: Maybe Int
-  , drumCount :: Maybe Int 
+  , drumCount :: Maybe Int
   , drumCarrierCount :: Maybe Int
   , fleetSType :: Array STypeReq
+  }
+
+type ResultPack (f :: * -> *) =
+  { flagShipLevel :: Boolean
+  , shipCount :: Boolean
+  , flagShipTypeOf :: f Boolean
+  , levelCount :: f Boolean
+  , drumCount :: f Boolean
+  , drumCarrierCount :: f Boolean
+  , fleetSType :: Array Boolean
   }
 
 type RequirementObject =
@@ -36,10 +47,12 @@ type RequirementObject =
   , shipCount :: Int
   , flagShipTypeOf :: Nullable (Array String)
   , levelCount :: Nullable Int
-  , drumCount :: Nullable Int 
+  , drumCount :: Nullable Int
   , drumCarrierCount :: Nullable Int
   , fleetSType :: Array STypeReqObj
   }
+
+type ResultObject = ResultPack Nullable
 
 dummyRequirementPack :: RequirementPack
 dummyRequirementPack =
@@ -70,8 +83,8 @@ fromExpeditionRequirement = foldl updatePack dummyRequirementPack
         FleetShipCount c -> p { shipCount = c }
 
 -- the point of having this is to go from PureScript world to JavaScript
-packToObj :: RequirementPack -> RequirementObject
-packToObj rp =
+requirementPackToObj :: RequirementPack -> RequirementObject
+requirementPackToObj rp =
     { flagShipLevel: rp.flagShipLevel
     , shipCount: rp.shipCount
     , flagShipTypeOf: toNullable ((map >>> map) showSType rp.flagShipTypeOf)
@@ -82,7 +95,7 @@ packToObj rp =
     }
   where
     cov :: STypeReq -> STypeReqObj
-    cov stReq = 
+    cov stReq =
         { stypeReqCount: stReq.stypeReqCount
         , stypeOneOf: map showSType stReq.stypeOneOf
         }
@@ -91,6 +104,56 @@ allRequirements :: Array { id :: Int,  val :: RequirementObject }
 allRequirements = map mk allExpeditionIds
   where
     mk v = { id: v
-           , val: (packToObj <<< fromExpeditionRequirement) 
-                    (getExpeditionRequirement v)
+           , val: getExpeditionRequirementObject v
            }
+
+getExpeditionRequirementPack :: Int -> RequirementPack
+getExpeditionRequirementPack eId = fromExpeditionRequirement (getExpeditionRequirement eId)
+
+getExpeditionRequirementObject :: Int -> RequirementObject
+getExpeditionRequirementObject = requirementPackToObj <<< getExpeditionRequirementPack
+
+checkWithRequirementPack :: forall a. RequirementPack -> Fleet a -> ResultPack Maybe
+checkWithRequirementPack req fleet = case uncons fleet of
+    Just { head: fs, tail: tl } ->
+      let flagShipLevel = fs.level >= req.flagShipLevel
+      in { flagShipLevel: flagShipLevel
+         , shipCount: length fleet >= req.shipCount
+         , flagShipTypeOf:
+             map (checkShipType fs.stype) req.flagShipTypeOf
+         , levelCount:
+             map (\lc -> sum (map (\s -> s.level) fleet) >= lc) req.levelCount
+         , drumCount:
+             map (\dc -> sum (map (\s -> s.drumCount) fleet) >= dc) req.drumCount
+         , drumCarrierCount:
+             map (\dcc -> count (\s -> s.drumCount > 0) fleet >= dcc) req.drumCarrierCount
+         , fleetSType:
+             map (\r ->
+                     count
+                       (\s -> checkShipType s.stype r.stypeOneOf)
+                       fleet
+                     >= r.stypeReqCount) req.fleetSType
+         }
+    Nothing ->
+      let toFalseF :: forall a f. (Functor f) => f a -> f Boolean
+          toFalseF = map (const false)
+      in { flagShipLevel: false
+         , shipCount: false
+         , flagShipTypeOf: toFalseF req.flagShipTypeOf
+         , levelCount: toFalseF req.levelCount
+         , drumCount: toFalseF req.drumCount
+         , drumCarrierCount: toFalseF req.drumCarrierCount
+         , fleetSType: toFalseF req.fleetSType
+         }
+  where
+    checkShipType :: SType -> Array SType -> Boolean
+    checkShipType st = any (eqSType st)
+    count pred = length <<< filter pred
+
+resultPackToObject :: ResultPack Maybe -> ResultPack Nullable
+resultPackToObject rp = rp
+    { flagShipTypeOf = toNullable rp.flagShipTypeOf
+    , levelCount = toNullable rp.levelCount
+    , drumCount = toNullable rp.drumCount
+    , drumCarrierCount = toNullable rp.drumCarrierCount
+    }
