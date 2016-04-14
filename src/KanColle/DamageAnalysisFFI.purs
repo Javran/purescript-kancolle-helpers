3d36c6a52e5d2e9f68adaad1000753e66843f97b
Javran Cheng <Javran.c@gmail.com>
2016-04-14 02:51:39 -0400

    complete FFI

--- src/KanColle/DamageAnalysisFFI.purs
+++ src/KanColle/DamageAnalysisFFI.purs
@@ -26,16 +26,48 @@ dc6 = replicate 6 Nothing
 dc12 :: Array (Maybe DameCon)
 dc12 = replicate 12 Nothing
 
-analyzeBattleJS :: Fn2 (Array Int) Battle { main :: Array (Nullable ShipResult)
-                                          , enemy :: Array (Nullable ShipResult)}
-analyzeBattleJS = mkFn2 (\ds b -> convert (analyzeBattle (readDameCon ds) b))
-  where
-    convert s = { main: map toNullable s.main
-                , enemy: map toNullable s.enemy }
+type FleetResult f = Array (f ShipResult)
 
-analyzeNightBattleJS :: Fn2 (Array Int) Battle { main :: Array (Nullable ShipResult)
-                                               , enemy :: Array (Nullable ShipResult)}
-analyzeNightBattleJS = mkFn2 (\ds b -> convert (analyzeNightBattle (readDameCon ds) b))
+type BattleResult f =
+  { main :: FleetResult f
+  , enemy :: FleetResult f
+  }
+  
+type CombinedBattleResult f =
+  { main :: FleetResult f
+  , enemy :: FleetResult f
+  , escort :: FleetResult f
+  }
+  
+liftToFFI :: (Array (Maybe DameCon) -> Battle -> BattleResult Maybe)
+          -> Fn2 (Array Int) Battle (BattleResult Nullable)
+liftToFFI f = mkFn2 (\ds b -> convert (f (readDameCon ds) b))
   where
     convert s = { main: map toNullable s.main
                 , enemy: map toNullable s.enemy }
+                
+liftToFFICombined :: (Array (Maybe DameCon) -> Battle -> CombinedBattleResult Maybe)
+                  -> Fn2 (Array Int) Battle (CombinedBattleResult Nullable)
+liftToFFICombined f = mkFn2 (\ds b -> convert (f (readDameCon ds) b))
+  where
+    convert s = { main: map toNullable s.main
+                , enemy: map toNullable s.enemy
+                , escort: map toNullable s.escort }
+
+analyzeBattleJS :: Fn2 (Array Int) Battle (BattleResult Nullable)
+analyzeBattleJS = liftToFFI analyzeBattle
+
+analyzeNightBattleJS :: Fn2 (Array Int) Battle (BattleResult Nullable)
+analyzeNightBattleJS = liftToFFI analyzeNightBattle
+
+analyzeSTFBattleJS :: Fn2 (Array Int) Battle (CombinedBattleResult Nullable)
+analyzeSTFBattleJS = liftToFFICombined analyzeSTFBattle
+
+analyzeCTFBattleJS :: Fn2 (Array Int) Battle (CombinedBattleResult Nullable)
+analyzeCTFBattleJS = liftToFFICombined analyzeCTFBattle
+
+analyzeTECFBattleJS :: Fn2 (Array Int) Battle (CombinedBattleResult Nullable)
+analyzeTECFBattleJS = liftToFFICombined analyzeTECFBattle
+
+analyzeCombinedNightBattleJS :: Fn2 (Array Int) Battle (BattleResult Nullable)
+analyzeCombinedNightBattleJS = liftToFFI analyzeCombinedNightBattle
