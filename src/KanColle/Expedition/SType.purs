cfcdf8c8569e4413c67726d00018d99de083ed3f
Javran Cheng <Javran.c@gmail.com>
2016-08-12 16:50:27 +0800

    SType specialized for expeditions

---
+++ src/KanColle/Expedition/SType.purs
@@ -0,0 +1,37 @@
+module KanColle.Expedition.SType
+  ( SType(..)
+  , matchSType
+  ) where
+
+import Prelude
+import KanColle.SType as K
+
+-- ship type specialized for expeditions
+data SType
+  = DD
+  | CL
+  | CVLLike
+  | SSLike
+  | CA
+  | BBV
+  | AS
+  | CT
+  | AV
+
+matchSType :: SType -> K.SType -> Boolean
+matchSType s ks = case s of
+    DD -> ks == K.DD
+    CL -> ks == K.CL
+    CVLLike ->
+        ks == K.CV
+     || ks == K.CVL
+     || ks == K.AV
+     || ks == K.CVB
+    SSLike ->
+        ks == K.SS
+     || ks == K.SSV
+    CA -> ks == K.CA
+    BBV -> ks == K.BBV
+    AS -> ks == K.AS
+    CT -> ks == K.CT
+    AV -> ks == K.AV
